9f09f2ec54c962e49889231bb2b9a97fca9e2c79
izabelladudaryk <izabelladudaryk@gmail.com>
2021-06-30 03:16:49 +0300

    update calendarActivity

--- app/src/main/java/com/disabella/mypolice/CalendarActivity.kt
+++ app/src/main/java/com/disabella/mypolice/CalendarActivity.kt
@@ -22,25 +22,33 @@ class CalendarActivity : AppCompatActivity() {
         setContentView(R.layout.activity_calendar)
 
 
-        //val dateOfComing = findViewById<EditText>(R.id.date_of_coming)
-        val dateOfDeparture: EditText = findViewById<EditText>(R.id.date_of_departure)
+        val dateOfComing = findViewById<EditText>(R.id.date_of_coming)
         val textViewCount = findViewById<TextView>(R.id.textview_count)
         val buttonCount = findViewById<Button>(R.id.button_count)
         val df: DateFormat = SimpleDateFormat("dd.MM.yyyy")
         val date: String = df.format(Calendar.getInstance().time)
 
         val todaysDate = df.parse(date)
-        val dateOfDepartureToDate = df.parse(dateOfDeparture.text.toString())
+        val dateOfComingToDate = df.parse(dateOfComing.text.toString())
 
-        val difference: Long = todaysDate.time - dateOfDepartureToDate.time
+        val difference: Long = todaysDate.time - dateOfComingToDate.time
         val days =
             (difference / (24 * 60 * 60 * 1000)).toInt() // миллисекунды / (24ч * 60мин * 60сек * 1000мс)
 
         buttonCount.setOnClickListener {
-            if (days > 0) {
-                textViewCount.text = "Your VISA will expire in $days days"
-            } else {
-                textViewCount.text = "Your VISA expired $days days ago"
+            var amountOfDays = 0
+            when {
+                days > 90 -> {
+                    amountOfDays = days - 90
+                    textViewCount.text = "This VISA expired $amountOfDays days ago"
+                }
+                days == 90 -> {
+                    textViewCount.text = "This visa will expire tomorrow"
+                }
+                else -> {
+                    amountOfDays = days - 90
+                    textViewCount.text = "This VISA will expire in $amountOfDays days"
+                }
             }
         }
     }
